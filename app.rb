87b259c367e41624e15776a9ac7b7350aaa7edd0
mdlp92 <michael.h.delapena@gmail.com>
2020-05-19 02:16:02 +0000

    app.rb done

--- app.rb
+++ app.rb
@@ -6,17 +6,25 @@ def view(template); erb template.to_sym; end
 get "/" do
   ### Get the weather
   # Evanston, Kellogg Global Hub... replace with a different location if you want
-  lat = 42.0574063
-  long = -87.6722787
+  lat = -8.5858
+  long = 118.7811
 
   units = "imperial" # or metric, whatever you like
-  key = "YOUR-API-KEY-GOES-HERE" # replace this with your real OpenWeather API key
+  key = "bb1e42b83f094c579178d96b912385c6" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
 
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
+
+  puts "It is currently #{@forecast["current"]["temp"]} degrees and #{@forecast["current"]["weather"][0]["description"]}"
+puts "Extended forecast:"
+day_number = 1
+for day in forecast["daily"]
+   puts "On day #{day_number}, A high of #{day["temp"]["max"]} and #{day["weather"][0]["description"]}"
+   day_number = day_number + 1
+end
 
   ### Get the news
 end
