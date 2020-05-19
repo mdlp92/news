b57e7fb973325e4121ca8039625f649917b0bee3
mdlp92 <michael.h.delapena@gmail.com>
2020-05-19 02:48:21 +0000

    got 7 day forecast working

--- app.rb
+++ app.rb
@@ -5,29 +5,33 @@ def view(template); erb template.to_sym; end
 
 get "/" do
   ### Get the weather
-  # Evanston, Kellogg Global Hub... replace with a different location if you want
+  # Bali weather
   lat = -8.5858
   long = 118.7811
 
   units = "imperial" # or metric, whatever you like
-  key = "bb1e42b83f094c579178d96b912385c6" # replace this with your real OpenWeather API key
+  key = "8ecb3366c20922d16e101245739fef2d" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
+  url = "https://api.openweathermap.org/data/2.5/onecall?lat=-8.5858&lon=118.7811&units=#{units}&appid=#{key}"
 
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
 
-  current_temp = @forecast["current"]["temp"]
-  forecast_temp = @forecast["current"]["weather"]["description"]
-
-  puts "It is currently #{@forecast["current"]["temp"]} degrees and #{@forecast["current"]["weather"]["description"]}"
+puts "It is currently #{@forecast["current"]["temp"]} degrees and #{@forecast["current"]["weather"][0]["description"]}"
 puts "Extended forecast:"
 day_number = 1
-for day in forecast["daily"]
+for day in @forecast["daily"]
    puts "On day #{day_number}, A high of #{day["temp"]["max"]} and #{day["weather"][0]["description"]}"
    day_number = day_number + 1
 end
 
+view "news"
   ### Get the news
 end
+
+key1 = "bb1e42b83f094c579178d96b912385c6"
+
+url = "https://newsapi.org/v2/top-headlines?country=id&apiKey=#{key1}"
+news = HTTParty.get(url).parsed_response.to_hash
+# news is now a Hash you can pretty print (pp) and parse for your output
