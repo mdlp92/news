980510e509dfce89b88d5755f573c6264b712dc2
mdlp92 <michael.h.delapena@gmail.com>
2020-05-19 03:32:32 +0000

    final changes to news

--- app.rb
+++ app.rb
@@ -29,15 +29,9 @@ end
 
 view "news"
   ### Get the news
-end
 
 ## beginning to now get the news
-
-get "/" do
 key2 = "bb1e42b83f094c579178d96b912385c6"
-req = open(url2)
-response_body = req.read
-
 
 url2 = "https://newsapi.org/v2/top-headlines?country=id&apiKey=#{key2}"
 news = HTTParty.get(url2).parsed_response.to_hash
@@ -45,7 +39,6 @@ news = HTTParty.get(url2).parsed_response.to_hash
 puts "Today's top stories are #{news["articles"]["source"]["name"]}"
 puts news["articles"]["description"]
 puts news["articles"]["url"]
-puts response_body
 
 end
 
