e72947388fed4cee001abf6cabc6754219840159
mdlp92 <michael.h.delapena@gmail.com>
2020-05-20 03:57:24 +0000

    got news hash to work

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ get "/" do
   key = "8ecb3366c20922d16e101245739fef2d" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=-8.5858&lon=118.7811&units=#{units}&appid=#{key}"
+  url0 = "https://api.openweathermap.org/data/2.5/onecall?lat=-8.5858&lon=118.7811&units=#{units}&appid=#{key}"
 
   # make the call
-  @forecast = HTTParty.get(url).parsed_response.to_hash
+  @forecast = HTTParty.get(url0).parsed_response.to_hash
 
 puts "It is currently #{@forecast["current"]["temp"]} degrees and #{@forecast["current"]["weather"][0]["description"]}"
 puts "Extended forecast:"
@@ -26,20 +26,28 @@ for day in @forecast["daily"]
    puts "On day #{day_number}, A high of #{day["temp"]["max"]} and #{day["weather"][0]["description"]}"
    day_number = day_number + 1
 end
-
-view "news"
   ### Get the news
 
 ## beginning to now get the news
 key2 = "bb1e42b83f094c579178d96b912385c6"
 
+#"https://newsapi.org/v2/top-headlines?country=id&apiKey=bb1e42b83f094c579178d96b912385c6"
 url2 = "https://newsapi.org/v2/top-headlines?country=id&apiKey=#{key2}"
-news = HTTParty.get(url2).parsed_response.to_hash
+news1 = HTTParty.get(url2).parsed_response.to_hash
 
-puts "Today's top stories are #{news["articles"]["source"]["name"]}"
-puts news["articles"]["description"]
-puts news["articles"]["url"]
+for articles in news1["articles"]
+    puts " today's news is #{articles["title"]}"
+    puts "by #{articles["author"]}"
+    puts "you can read more here: #{articles["url"]}"
+
+# puts  news1["articles"][0]["url"]
+# puts  news1["articles"][1]["url"]
+# puts  news1["articles"][3]["url"]
+# puts news["articles"]["description"]
+# puts news["articles"]["url"]
+
+view "news"
 
 end
-
+end
 # news is now a Hash you can pretty print (pp) and parse for your output
